40e0c49f46a971f36fc0b4856894142ce3792cfe
Rishop Babu <rishopbabu22@gmail.com>
2023-08-22 20:59:54 +0530

    crud operations implemented

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException, Response
 from pydantic import BaseModel
 from random import randrange
 
@@ -17,11 +17,19 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+
 # Find individual post
 def find_post(id):
     for posts in my_posts:
         if posts["id"] == id:
             return posts
+
+
+# find index of the post
+def find_index_of_post(id):
+    for i, posts in enumerate(my_posts):
+        if posts['id'] == id:
+            return i
 
 # CRUD Operations for Posts
 # Get all posts
@@ -29,18 +37,45 @@ def find_post(id):
 def get_posts():
     return {"data": my_posts}
 
+
 # Get an individual post based on id
-@app.get("/posts/{id}")
+@app.get("/post/{id}")
 def get_posts(id: int):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail = f"post with {id} does not found")
     return {"post_details": post}
-    
+
 
 # Post a single posts
-@app.post("/posts")
+@app.post("/createpost", status_code= status.HTTP_201_CREATED)
 def create_post(post: Post):
    post_dict = post.model_dump()
    post_dict['id'] = randrange(0,100)
    my_posts.append(post_dict)
    return {"data": post_dict}
 
+
+# Update a post
+@app.put("/updatepost/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_of_post(id)
+    if index == None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail = f"post with {id} does not found")
+    update_post = post.model_dump()
+    update_post['id'] = id
+    my_posts[index] = update_post
+    return {"data": update_post}
+
+
+# Delete a single post
+@app.delete("/deletepost/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_of_post(id)
+    if index == None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail = f"post with {id} does not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
