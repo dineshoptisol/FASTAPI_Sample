cd277c8506fdb61fa45272e68b447d5afcbd63aa
Rishop Babu <rishopbabu22@gmail.com>
2023-08-22 19:48:00 +0530

    updated with basemodel

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
+from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to my api"}
+# Global variable to store posts locally
+my_posts = [{"title": "FAST API 1", "content": "This is my first time building an API 1", "id": 1},
+            {"title": "FAST API 2", "content": "This is my first time building an API 2", "id": 2} ]
 
+# Base model for Posts
+# This BaseModel always returns as Dictonary
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+# Find individual post
+def find_post(id):
+    for posts in my_posts:
+        if posts["id"] == id:
+            return posts
+
+# CRUD Operations for Posts
+# Get all posts
 @app.get("/posts")
-def get_users():
-    return {"data": "All users are here"}
+def get_posts():
+    return {"data": my_posts}
 
-@app.post("/createpost")
-def create_post(payload: dict = Body()):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+# Get an individual post based on id
+@app.get("/posts/{id}")
+def get_posts(id: int):
+    post = find_post(id)
+    return {"post_details": post}
+    
 
-@app.delete("/deleteuser")
-def delete_user():
-    return {"message": "User deleted"}
+# Post a single posts
+@app.post("/posts")
+def create_post(post: Post):
+   post_dict = post.model_dump()
+   post_dict['id'] = randrange(0,100)
+   my_posts.append(post_dict)
+   return {"data": post_dict}
 
-app.head("/headapi")
-def head_api():
-    return {"message": "head api"}
-
-app.put("/putapi")
-def put_api():
-    return {"message": "put api"}
